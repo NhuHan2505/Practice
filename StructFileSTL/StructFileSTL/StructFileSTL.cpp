7bd208950ed95c035438830b59808e14c3f37571
NhuHan2505 <nhuhan2505@gmail.com>
2019-11-21 14:16:23 +0700

    inputStudent

--- StructFileSTL/StructFileSTL/StructFileSTL.cpp
+++ StructFileSTL/StructFileSTL/StructFileSTL.cpp
@@ -14,15 +14,32 @@ struct Student
 	string name;
 	float score;
 };
+void inputStudent(Student std)
+{
+	cout << "Input Student's ID: ";
+	cin >> std.id;
+	cout << "Input Student's Name: ";
+	cin >> std.name;
+	do
+	{
+		cout << "Input Student's Score (must be in range 0-10): ";
+		cin >> std.score;
+	} while (std.score < 0 || std.score>10);
+}
 
 void outputStudent(Student std)
 {
 	cout << "ID: " << std.id << "\t" << "Name: " << std.name << "\t" << "Score: " << std.score << endl;
 }
-
+void showVector(vector <Student> std)
+{
+	for (int i = 0; i < std.size(); i++) outputStudent(std[i]);
+}
 int main()
 {
 	vector <Student> std;
+	Student a;
+	inputStudent(a);
     return 0;
 }
 

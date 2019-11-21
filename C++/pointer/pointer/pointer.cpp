1cf3d526ab782f1f7cd55c5bad9108544275e400
NhuHan2505 <nhuhan2505@gmail.com>
2019-11-21 10:30:05 +0700

    modified pointer and add pointer2&4

--- C++/pointer/pointer/pointer.cpp
+++ C++/pointer/pointer/pointer.cpp
@@ -8,16 +8,17 @@ typedef int *IntPtrType;
 
 int main()
 {
-	//ptr_c is a pointer 
+	//ptr_a, ptr_b là con trỏ
+	//ptr_c là biến 
 	IntPtrType ptr_a, ptr_b, *ptr_c;
 	
-	ptr_a = new int; //ptr_a là biến kiểu int
+	ptr_a = new int; //ptr_a là con trỏ kiểu int
 	*ptr_a = 3; //3 là giá trị của con trỏ ptr_a
 	ptr_b = ptr_a; //gán giá trị của ptr_a cho ptr_b (tức là 3)
 	//in ra giá trị của con trỏ ptr_a và ptr_b là 3
 	cout << *ptr_a << " " << *ptr_b << "\n";
 	
-	ptr_b = new int;// ptr_b là biến kiểu int
+	ptr_b = new int;// ptr_b là con trỏ kiểu int
 	*ptr_b = 9; //gán giá trị tại địa chỉ ptr_b là 9
 	//in ra giá trị của con trỏ ptr_a là 3 và giá trị của con trỏ ptr_b vừa gán là 9
 	cout << *ptr_a << " " << *ptr_b << "\n";
@@ -33,7 +34,7 @@ int main()
 	cout << *ptr_a << " " << *ptr_b << "\n";
 	
 	//&ptr_a là địa chỉ của biến ptr_a
-	ptr_c = &ptr_a; //gán giá trị địa chỉ của ptr_a cho con trỏ ptr_C
+	ptr_c = &ptr_a; //gán giá trị địa chỉ của ptr_a cho ptr_C
 	cout << *ptr_c << " " << **ptr_c << "\n";
 	//*ptr_c là địa chỉ của biến ptr_a
 	//**ptr_c là giá trị của biến con trỏ ptr_c trỏ tới
